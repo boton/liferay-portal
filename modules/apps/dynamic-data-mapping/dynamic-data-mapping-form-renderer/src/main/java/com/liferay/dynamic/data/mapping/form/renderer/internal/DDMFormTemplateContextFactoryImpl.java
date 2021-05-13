af33407f2938ab2e6fb1b956794eeba75b696f5a
Jorge González <jorge.gonzalez@liferay.com>
2021-05-13 13:53:42 +0200

    LPS-130271 Add a new taglib attribute submittable to avoid submit when is not needed

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/DDMFormTemplateContextFactoryImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/DDMFormTemplateContextFactoryImpl.java
@@ -276,6 +276,9 @@ public class DDMFormTemplateContextFactoryImpl
 		templateContext.put("submitLabel", submitLabel);
 
 		templateContext.put(
+			"submittable", ddmFormRenderingContext.isSubmittable());
+
+		templateContext.put(
 			"templateNamespace", getTemplateNamespace(ddmFormLayout));
 		templateContext.put("viewMode", ddmFormRenderingContext.isViewMode());
 
